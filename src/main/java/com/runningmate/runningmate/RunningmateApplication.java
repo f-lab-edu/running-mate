40b775816bab2d48dfae7cc656d834e90be496ae
YoonWonJun <ywj036@naver.com>
2021-11-13 15:13:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/runningmate/runningmate/RunningmateApplication.java
@@ -0,0 +1,13 @@
+package com.runningmate.runningmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningmateApplication.class, args);
+	}
+
+}
