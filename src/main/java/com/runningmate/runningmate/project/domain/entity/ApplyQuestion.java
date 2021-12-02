a84400f6e05d72ccbc71fa109ac813891b3b0aae
YoonWonJun <ywj036@naver.com>
2021-12-02 22:45:07 +0900

    프로젝트 등록 기능 개발

---
+++ src/main/java/com/runningmate/runningmate/project/domain/entity/ApplyQuestion.java
@@ -0,0 +1,20 @@
+package com.runningmate.runningmate.project.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyQuestion {
+    private long applyQuestionId;
+    private long projectId;
+    private String question;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+}
---
+++ src/main/java/com/runningmate/runningmate/project/dto/ApplyQuestionSaveRequestDto.java
@@ -0,0 +1,17 @@
+package com.runningmate.runningmate.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyQuestionSaveRequestDto {
+    @NotBlank(message = "질문을 입력해주세요")
+    private String question;
+}
