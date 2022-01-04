5cb86ae7d555fc713a527ac2ba55c55bdf420e0c
YoonWonJun <ywj036@naver.com>
2022-01-04 19:02:57 +0900

    댓글 입력/수정 요청 시 글자 수 제한

--- src/main/java/com/runningmate/runningmate/comment/dto/request/CommentUpdateRequestDto.java
+++ src/main/java/com/runningmate/runningmate/comment/dto/request/CommentUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package com.runningmate.runningmate.comment.dto.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class CommentUpdateRequestDto {
 
     @NotBlank(message = "내용을 입력해주세요.")
+    @Size(max = 250, message = "250자 이상 작성할 수 없습니다.")
     private String contents;
 }
