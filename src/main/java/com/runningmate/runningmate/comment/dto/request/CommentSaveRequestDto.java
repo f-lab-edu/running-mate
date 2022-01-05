d7f81452aaf4e214c8b51be5313eb037b443b2d5
YoonWonJun <ywj036@naver.com>
2022-01-05 15:18:52 +0900

    프로젝트 댓글 테스트 코드 작성

--- src/main/java/com/runningmate/runningmate/comment/dto/request/CommentSaveRequestDto.java
+++ src/main/java/com/runningmate/runningmate/comment/dto/request/CommentSaveRequestDto.java
@@ -1,6 +1,5 @@
 package com.runningmate.runningmate.comment.dto.request;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
