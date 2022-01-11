7b84546957cf199bf275f2ef68113bbe471750ef
junsoo90 <asdfrf1@daum.net>
2022-01-12 02:07:07 +0900

    Master(insert,update,delete) / Slave(select) 분리를 위한 Config 추가 및 설정파일 추가

--- src/main/java/com/runningmate/runningmate/configuration/DataBaseConfiguration.java
+++ src/main/java/com/runningmate/runningmate/configuration/DataBaseConfiguration.java
@@ -1,13 +1,18 @@
 package com.runningmate.runningmate.configuration;
 
+import com.runningmate.runningmate.dataSource.DataSourceType;
+import com.runningmate.runningmate.dataSource.ReplicationRoutingDataSource;
 import com.runningmate.runningmate.image.domain.entity.ImageStatus;
 import com.runningmate.runningmate.project.domain.entity.ProjectApplyStatus;
 import com.runningmate.runningmate.user.entity.UserStatus;
 import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.TypeHandler;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
@@ -15,20 +20,53 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 
 @Configuration
 public class DataBaseConfiguration {
 
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
+    @Bean(name = "masterDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource masterDataSource() {
         return DataSourceBuilder
             .create()
             .type(HikariDataSource.class)
             .build();
     }
 
+    @Bean(name = "slaveDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder
+            .create()
+            .type(HikariDataSource.class)
+            .build();
+    }
+
+
+    @Bean(name = "routingDataSource")
+    public DataSource routingDataSource(
+        @Qualifier("masterDataSource") final DataSource masterDataSource,
+        @Qualifier("slaveDataSource") final DataSource slaveDataSource) {
+
+        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.Master, masterDataSource);
+        targetDataSources.put(DataSourceType.Slave, slaveDataSource);
+
+        routingDataSource.setTargetDataSources(targetDataSources);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+        return routingDataSource;
+    }
+
+    @Primary
+    @Bean(name = "routingLazyDataSource")
+    public DataSource routingLazyDataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
 
     @Bean(name = "sessionFactory")
     public SqlSessionFactory sessionFactory(DataSource dataSource, ApplicationContext applicationContext) throws Exception {
